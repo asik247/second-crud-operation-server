590684d23c40492853e56c3aa007118a57357c9e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-18 23:13:43 +0600

    root api done

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 // const cros = require('cros');
 const app = express();
 const port = process.env.PORT || 3000;
-// MiddleWare;
-
-// Root Api / ;
+//? MiddleWare;
+// TODO Root Api / ;
 app.get('/',(req,res)=>{
     res.send("Root Server /")
 })
-// Listinner;
+//! Listinner;
 app.listen(port,()=>{
     console.log(`This server runing in port:${port}`);
 })
-
+//! 9WwxBHCQRsqx3Ozi  secondCrudUsers
